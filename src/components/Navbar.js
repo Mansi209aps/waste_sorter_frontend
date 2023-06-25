ed5df1b641d6135c0f9a70fd3a271f12f86a2391
Ritik Raj <ritik2358@gmail.com>
2023-06-26 01:26:35 +0530

    some changes in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export const Navbar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     return (
-        <div class="bg-gray-900">
+        <div class="bg-deep-purple-accent-400">
             <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
                 <div class="relative flex items-center justify-between">
                     <a
@@ -14,21 +14,7 @@ export const Navbar = () => {
                         title="Company"
                         class="inline-flex items-center"
                     >
-                        <svg
-                            class="w-8 text-teal-accent-400"
-                            viewBox="0 0 24 24"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeMiterlimit="10"
-                            stroke="currentColor"
-                            fill="none"
-                        >
-                            <rect x="3" y="1" width="7" height="12" />
-                            <rect x="3" y="17" width="7" height="6" />
-                            <rect x="14" y="1" width="7" height="6" />
-                            <rect x="14" y="11" width="7" height="12" />
-                        </svg>
+
                         <span class="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">
                             Strasbourg waste sorter
                         </span>
