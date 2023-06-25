698b912019a40161fd13a7b84f4a17c6178513e7
Ritik Raj <ritik2358@gmail.com>
2023-06-26 01:43:42 +0530

    final navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export const Navbar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     return (
-        <div class="bg-deep-purple-accent-400">
+        <div class="bg-green-700">
             <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
                 <div class="relative flex items-center justify-between">
                     <a
@@ -14,79 +14,11 @@ export const Navbar = () => {
                         title="Company"
                         class="inline-flex items-center"
                     >
-                        <svg
-                            class="w-8 text-teal-accent-400"
-                            viewBox="0 0 24 24"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeMiterlimit="10"
-                            stroke="currentColor"
-                            fill="none"
-                        >
-                            <rect x="3" y="1" width="7" height="12" />
-                            <rect x="3" y="17" width="7" height="6" />
-                            <rect x="14" y="1" width="7" height="6" />
-                            <rect x="14" y="11" width="7" height="12" />
-                        </svg>
+
                         <span class="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">
-                            Company
+                            Strasbourg waste sorter
                         </span>
                     </a>
-                    <ul class="flex items-center hidden space-x-8 lg:flex">
-                        <li>
-                            <a
-                                href="/"
-                                aria-label="Our product"
-                                title="Our product"
-                                class="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
-                            >
-                                Product
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="/"
-                                aria-label="Our product"
-                                title="Our product"
-                                class="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
-                            >
-                                Features
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="/"
-                                aria-label="Product pricing"
-                                title="Product pricing"
-                                class="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
-                            >
-                                Pricing
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="/"
-                                aria-label="About us"
-                                title="About us"
-                                class="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
-                            >
-                                About us
-                            </a>
-                        </li>
-                    </ul>
-                    <ul class="flex items-center hidden space-x-8 lg:flex">
-                        <li>
-                            <a
-                                href="/"
-                                class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-                                aria-label="Sign up"
-                                title="Sign up"
-                            >
-                                Sign up
-                            </a>
-                        </li>
-                    </ul>
                     <div class="lg:hidden">
                         <button
                             aria-label="Open Menu"
