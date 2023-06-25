37ff33a39f948499f1ced9e63c09a19678d0151a
Ritik Raj <ritik2358@gmail.com>
2023-06-26 00:48:16 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Navbar } from './components/Navbar';
+import { Header } from './components/Header';
 
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Header />
+    </>
+
+
   );
 }
 
