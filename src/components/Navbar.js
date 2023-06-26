6770531b966cdb940938a208ad449cf3114c1006
Mansi <mansisharma209aps@gmail.com>
2023-06-26 09:52:24 +0530

    sticky navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import { useState } from 'react';
+// import { useState } from 'react';
 
 export const Navbar = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    // const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     return (
-        <div class="bg-green-700">
-            <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-                <div class="relative flex items-center justify-between">
-                    <a
-                        href="/"
-                        aria-label="Company"
-                        title="Company"
-                        class="inline-flex items-center"
-                    >
+        <nav className='sticky top-0 z-30 w-full'>
+            <div class="bg-green-700">
+                <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+                    <div class="relative flex items-center justify-between">
+                        <a
+                            href="/"
+                            aria-label="Company"
+                            title="Company"
+                            class="inline-flex items-center"
+                        >
 
-                        <span class="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">
-                            Strasbourg waste sorter
-                        </span>
-                    </a>
-                    <div class="lg:hidden">
+                            <span class="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">
+                                Strasbourg waste sorter
+                            </span>
+                        </a>
+                        {/* <div class="lg:hidden">
                         <button
                             aria-label="Open Menu"
                             title="Open Menu"
@@ -87,8 +88,8 @@ export const Navbar = () => {
                                                 </svg>
                                             </button>
                                         </div>
-                                    </div>
-                                    <nav>
+                                    </div> */}
+                        {/* <nav>
                                         <ul class="space-y-4">
                                             <li>
                                                 <a
@@ -141,13 +142,12 @@ export const Navbar = () => {
                                                 </a>
                                             </li>
                                         </ul>
-                                    </nav>
-                                </div>
-                            </div>
-                        )}
+                                    </nav> */}
                     </div>
                 </div>
             </div>
-        </div>
-    );
-};
+        </nav>
+
+    )
+}
+
