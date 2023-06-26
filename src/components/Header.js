72ae63914e17a67114384d7f6ef94820c04b3fdb
Mansi <mansisharma209aps@gmail.com>
2023-06-26 10:12:54 +0530

    basic header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,57 +1,73 @@
-import { React } from 'react';
+import { React } from "react";
+
 export const Header = () => {
     return (
-        <div className="relative flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0">
-            <div className="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-full lg:absolute xl:px-0">
-                <svg
-                    className="absolute left-0 hidden h-full text-white transform -translate-x-1/2 lg:block"
-                    viewBox="0 0 100 100"
-                    fill="currentColor"
-                    preserveAspectRatio="none slice"
-                >
-                    <path d="M50 0H100L50 100H0L50 0Z" />
-                </svg>
-                <img
-                    className="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none md:h-96 lg:h-full"
-                    src="https://images.pexels.com/photos/3184291/pexels-photo-3184291.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-                    alt=""
-                />
-            </div>
-            <div className="relative flex flex-col items-start w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:max-w-screen-xl">
-                <div className="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
+        // Text content
+        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+            <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+                <div>
                     <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
-                        Brand new
+                        v0.1.0
                     </p>
-                    <h2 className="mb-5 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
-                        Everything you
-                        <br className="hidden md:block" />
-                        can imagine{' '}
-                        <span className="inline-block text-deep-purple-accent-400">
-                            is real
-                        </span>
-                    </h2>
-                    <p className="pr-5 mb-5 text-base text-gray-700 md:text-lg">
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-                        quae. explicabo.
-                    </p>
-                    <div className="flex items-center">
-                        <a
-                            href="/"
-                            className="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                </div>
+                <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+                    <span className="relative inline-block">
+                        <svg
+                            viewBox="0 0 52 24"
+                            fill="currentColor"
+                            className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
                         >
-                            Get started
-                        </a>
-                        <a
-                            href="/"
-                            aria-label=""
-                            className="inline-flex items-center font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700"
-                        >
-                            Learn more
-                        </a>
-                    </div>
+                            <defs>
+                                <pattern
+                                    id="679d5905-e08c-4b91-a66c-84aefbb9d2f5"
+                                    x="0"
+                                    y="0"
+                                    width=".135"
+                                    height=".30"
+                                >
+                                    <circle cx="1" cy="1" r=".7" />
+                                </pattern>
+                            </defs>
+                            <rect
+                                fill="url(#679d5905-e08c-4b91-a66c-84aefbb9d2f5)"
+                                width="52"
+                                height="24"
+                            />
+                        </svg>
+                        <span className="relative">SELECT</span>
+                    </span>{' '}
+                    IMAGE OF TRASH ITEM TO UPLOAD
+                </h2>
+                <p className="text-base text-gray-700 md:text-lg">
+                    this is an early demo, so the model may be wrong a lot of times
+                </p>
+
+                {/* waste_sorter_image */}
+            </div>
+            <div className="mx-auto lg:max-w-2xl">
+                <div className="relative w-full transition-shadow duration-300 hover:shadow-xl">
+                    <img
+                        className="object-cover w-full h-56 rounded shadow-lg sm:h-64 md:h-80 lg:h-96"
+                        src="https://images.pexels.com/photos/927022/pexels-photo-927022.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+                        alt=""
+                    />
+                    {/* <a
+                        href="/"
+                        aria-label="Play Video"
+                        className="absolute inset-0 flex items-center justify-center w-full h-full transition-colors duration-300 bg-gray-900 bg-opacity-50 group hover:bg-opacity-25"
+                    > */}
+                    {/* <div className="flex items-center justify-center w-16 h-16 transition duration-300 transform bg-gray-100 rounded-full shadow-2xl group-hover:scale-110">
+                            <svg
+                                className="w-10 text-gray-900"
+                                fill="currentColor"
+                                viewBox="0 0 24 24"
+                            >
+                                <path d="M16.53,11.152l-8-5C8.221,5.958,7.833,5.949,7.515,6.125C7.197,6.302,7,6.636,7,7v10 c0,0.364,0.197,0.698,0.515,0.875C7.667,17.958,7.833,18,8,18c0.184,0,0.368-0.051,0.53-0.152l8-5C16.822,12.665,17,12.345,17,12 S16.822,11.335,16.53,11.152z" />
+                            </svg>
+                        </div> */}
+                    {/* </a> */}
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
